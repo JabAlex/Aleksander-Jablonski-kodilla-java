81e252770a694f394640500b1beea3a296e71b49
Aleksander Jablonski <jablonski.aleksander98@gmail.com>
2022-11-09 12:12:19 +0100

    6.2 Added Calculator and SimpleUser tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - kalkulator:");
+
+        Calculator calculator = new Calculator();
+        int addResult = calculator.add(231, 114);
+        int subtractResult = calculator.subtract(231, 114);
+
+        if(addResult == (231 + 114) && subtractResult == (231 - 114)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
